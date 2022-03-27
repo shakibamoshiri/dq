6c0da5c0e2c30e8feda30c90e05bf5b9ae3382bb
Shakiba Moshiri <hi@shakiba.net>
2022-03-27 20:30:01 +0430

    Add check-docker-api.sh

---
+++ check-docker-api.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# bash script mode
+set -euo pipefail
+# debug mode
+# set -x
+
+function __error_handing__(){
+    local last_status_code=$1;
+    local error_line_number=$2;
+    echo 1>&2 "Error - exited with status $last_status_code at line $error_line_number";
+}
+
+trap  '__error_handing__ $? $LINENO' ERR
+
+readonly docker_socket='/var/run/docker.sock';
+
+# check for existence of needed commands
+which docker > /dev/null 2>&1
+which jq > /dev/null 2>&1
+which curl > /dev/null 2>&1 
+
+test -S $docker_socket > /dev/null 2>&1
+
+docker_api_version=$(curl --silent --unix-socket /var/run/docker.sock http://localhost/version | jq -r '.ApiVersion')
+echo "Docker API Version:"
+echo $docker_api_version
+echo "Docker ID:"
+curl --silent --unix-socket /var/run/docker.sock http://localhost/v$docker_api_version/info | jq -r '.ID'
